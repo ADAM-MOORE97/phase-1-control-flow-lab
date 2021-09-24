09c5bec14bc8127f2acfe0016681c601150d9be6
Adam <adammoore118@outlook.com>
2021-09-24 12:29:57 -0400

    commit tree 65953c5c429b584ae9a54569c1e0b922c38d2943

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+if (someValue >= 2501) {
+  return 'No can do.'
+} else if (someValue > 2000) { 
+  return 'I will gladly take your thirty bucks.'
+} else if (someValue <= 400) {
+  return 'This one is on me!'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.'
 }
+
+// function switchOnCharmFromTip(tip){
+//   if(tip ==='thanks for everything') {
+//     return 'Bye.';
+//   }
+//   if (tip ==='not as generous') {
+//     return 'Thank you.';
+//   } 
+//   if (tip ==='generous') {
+//     return 'Thank you so much.';
+//   } 
+// }
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip){  
+    case 'generous':
+      return 'Thank you so much.';
+    
+    case 'not as generous':
+      return 'Thank you.';
+      
+    default:
+      return 'Bye.'
+
+}
+
+}
+
+
+// switch (switchOnCharmFromTip) {
+//   case 'generous':
+//     'Thank you so much.';
+//   break;
+//   case 'not as generous':
+//      'Thank you.';
+//   break;
+//   default:
+//      'Bye.'
+
+// }
